4fc57a42f15623c2d977e0ee3d5d4e96b0e17f15
Shivam Sherkar <shivam23sherkar@gmail.com>
2020-09-16 14:17:07 +0530

    change

--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q ='
+            + city + '&appid = 00b9204d381eebe0caf5d86e632316b5').read()
+
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+            + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, "main/index.html", data)
